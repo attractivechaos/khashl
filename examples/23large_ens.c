b9a64a0b84ae431b7f1470ade119bd40dfe4cc2d
Attractive Chaos <attractor@live.co.uk>
2024-05-05 22:59:45 -0400

    track timing and peak RSS

--- examples/23large_ens.c
+++ examples/23large_ens.c
@@ -5,6 +5,24 @@
 
 KHASHE_MAP_INIT(KH_LOCAL, map64_t, map64, uint64_t, uint64_t, kh_hash_uint64, kh_eq_generic)
 
+static double udb_cputime(void)
+{
+	struct rusage r;
+	getrusage(RUSAGE_SELF, &r);
+	return r.ru_utime.tv_sec + r.ru_stime.tv_sec + 1e-6 * (r.ru_utime.tv_usec + r.ru_stime.tv_usec);
+}
+
+static long udb_peakrss(void)
+{
+	struct rusage r;
+	getrusage(RUSAGE_SELF, &r);
+#ifdef __linux__
+	return r.ru_maxrss * 1024;
+#else
+	return r.ru_maxrss;
+#endif
+}
+
 uint64_t splitmix64(uint64_t *x)
 {
 	uint64_t z = ((*x) += 0x9e3779b97f4a7c15ULL);
@@ -15,11 +33,14 @@ uint64_t splitmix64(uint64_t *x)
 
 int main(int argc, char *argv[])
 {
-	int i, n = 10000000;
+	int i, n = 20000000;
 	uint64_t x = 11, sum = 0;
 	map64_t *h;
 	kh_ensitr_t k;
+	double t;
+
 	if (argc > 1) n = atol(argv[1]);
+	t = udb_cputime();
 	h = map64_init(6);
 	for (i = 0; i < n; ++i) {
 		uint64_t z, key;
@@ -33,7 +54,10 @@ int main(int argc, char *argv[])
 	kh_ens_foreach(h, k) {
 		sum += kh_ens_val(h, k);
 	}
-	printf("%llx\n", sum);
+	printf("elements: %lu\n", (long)kh_ens_size(h));
+	printf("checksum: %llx\n", sum);
+	printf("CPU time: %.3f sec\n", udb_cputime() - t);
+	printf("Peak RSS: %.3f MB\n", udb_peakrss() / 1024. / 1024.);
 	map64_destroy(h);
 	return 0;
 }
